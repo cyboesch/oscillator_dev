7afa12c9e349c2c2a3b067fab0bb2e0985effe09
Geoffrey Roeder <roeder@princeton.edu>
2024-09-04 16:41:04 -0400

    grad tests written and passing

--- sde-dev/artifacts/cld.py
+++ sde-dev/artifacts/cld.py
@@ -2,33 +2,36 @@ import jax.numpy as jnp
 import equinox as eqx
 import diffrax
 
+
 class CriticallyDampedLangevinDiffusion(eqx.Module):
     d: int
     M: float
     Gamma: float
+    gamma: float
     beta: float
-    T: float
 
-    def __init__(self, d: int, M: float, beta: float, T: float = 1.0):
-        if d <= 0 or M <= 0 or beta <= 0 or T <= 0:
-          raise ValueError("All parameters must be positive.")
+    def __init__(self, d: int, M: float, beta: float, gamma: float = 1.0):
+        if d <= 0 or M <= 0 or beta <= 0:
+            raise ValueError("All parameters must be positive.")
         self.d = d
         self.M = M
         self.Gamma = jnp.sqrt(4 * M)
+        self.gamma = gamma
         self.beta = beta
-        self.T = T
 
     def drift(self, t, u, args):
-        x, v = u[:self.d], u[self.d:]
+        x, v = u[: self.d], u[self.d :]
         f_x = self.beta * self.M**-1 * v
         f_v = -self.beta * x - self.beta * self.Gamma * self.M**-1 * v
         return jnp.concatenate([f_x, f_v])
 
     def diffusion(self, t, u, args):
-        G = jnp.zeros((2*self.d, 2*self.d))
-        G = G.at[self.d:, self.d:].set(jnp.sqrt(2 * self.Gamma * self.beta) * jnp.eye(self.d))
+        G = jnp.zeros((2 * self.d, 2 * self.d))
+        G = G.at[self.d :, self.d :].set(
+            jnp.sqrt(2 * self.Gamma * self.beta) * jnp.eye(self.d)
+        )
         return G
-      
+
     def get_terms(self, bm):
         drift_term = diffrax.ODETerm(self.drift)
         diffusion_term = diffrax.ControlTerm(self.diffusion, bm)
@@ -40,43 +43,109 @@ class CriticallyDampedLangevinDiffusion(eqx.Module):
     def mean(self, x_0, v_0, t):
         B_t = self.B(t)
         exp_term = jnp.exp(-2 * B_t / self.Gamma)
-        
-        x_t = (2 * B_t / self.Gamma * x_0 + 4 * B_t / self.Gamma**2 * v_0 + x_0) * exp_term
+
+        x_t = (
+            2 * B_t / self.Gamma * x_0 + 4 * B_t / self.Gamma**2 * v_0 + x_0
+        ) * exp_term
         v_t = (-B_t * x_0 - 2 * B_t / self.Gamma * v_0 + v_0) * exp_term
-        
+
         return jnp.concatenate([x_t, v_t])
 
     def covariance(self, t, Sigma_0_xx, Sigma_0_vv):
         B_t = self.B(t)
         exp_term = jnp.exp(-4 * B_t / self.Gamma)
         exp_term_plus = jnp.exp(4 * B_t / self.Gamma)
-        
-        Sigma_xx = (Sigma_0_xx + exp_term_plus - 1 + 
-                    4*B_t/self.Gamma*(Sigma_0_xx-1) + 
-                    4*B_t**2/self.Gamma**2*(Sigma_0_xx-2) + 
-                    16*B_t**2/self.Gamma**4*Sigma_0_vv) * exp_term
-        
-        Sigma_xv = (-B_t*Sigma_0_xx + 4*B_t/self.Gamma**2*Sigma_0_vv - 
-                    2*B_t**2/self.Gamma*(Sigma_0_xx-2) - 
-                    8*B_t**2/self.Gamma**3*Sigma_0_vv) * exp_term
-        
-        Sigma_vv = (self.Gamma**2/4*(exp_term_plus-1) + B_t*self.Gamma + 
-                    Sigma_0_vv*(1+4*B_t**2/self.Gamma**2-4*B_t/self.Gamma) + 
-                    B_t**2*(Sigma_0_xx-2)) * exp_term
-        
+
+        Sigma_xx = (
+            Sigma_0_xx
+            + exp_term_plus
+            - 1
+            + 4 * B_t / self.Gamma * (Sigma_0_xx - 1)
+            + 4 * B_t**2 / self.Gamma**2 * (Sigma_0_xx - 2)
+            + 16 * B_t**2 / self.Gamma**4 * Sigma_0_vv
+        ) * exp_term
+
+        Sigma_xv = (
+            -B_t * Sigma_0_xx
+            + 4 * B_t / self.Gamma**2 * Sigma_0_vv
+            - 2 * B_t**2 / self.Gamma * (Sigma_0_xx - 2)
+            - 8 * B_t**2 / self.Gamma**3 * Sigma_0_vv
+        ) * exp_term
+
+        Sigma_vv = (
+            self.Gamma**2 / 4 * (exp_term_plus - 1)
+            + B_t * self.Gamma
+            + Sigma_0_vv * (1 + 4 * B_t**2 / self.Gamma**2 - 4 * B_t / self.Gamma)
+            + B_t**2 * (Sigma_0_xx - 2)
+        ) * exp_term
+
         Sigma_t = jnp.array([[Sigma_xx, Sigma_xv], [Sigma_xv, Sigma_vv]])
         return jnp.kron(Sigma_t, jnp.eye(self.d))
 
-    def perturbation_kernel_dsm(self, u_0, t):
-        # “when conditioning on initial data and velocity samples x0 and v0 (as in denoising score matching (DSM)), the mean and covariance matrix of the perturbation kernel p(ut|u0) can be obtained by setting μ0 = (x0, v0)>, Σ0xx = 0, and Σ0vv = 0.” ([Dockhorn et al., 2022, p. 21](zotero://select/library/items/EW8U6A8H)) ([pdf](zotero://open-pdf/library/items/NAYANTYJ?page=21&annotation=6ZWQTEZZ))
-        x_0, v_0 = u_0[:self.d], u_0[self.d:]
+    def get_dsm_kernel_params(self, u_0, t):
+        """“when conditioning on initial data and velocity samples x0 and v0 (as in denoising score matching (DSM)), the mean and covariance matrix of the perturbation kernel p(ut|u0) can be obtained by setting μ0 = (x0, v0)>, Σ0xx = 0, and Σ0vv = 0.” ([Dockhorn et al., 2022, p. 21](zotero://select/library/items/EW8U6A8H)) ([pdf](zotero://open-pdf/library/items/NAYANTYJ?page=21&annotation=6ZWQTEZZ))"""
+        x_0, v_0 = u_0[: self.d], u_0[self.d :]
         mean = self.mean(x_0, v_0, t)
         cov = self.covariance(t, Sigma_0_xx=0, Sigma_0_vv=0)
         return mean, cov
 
-    def perturbation_kernel_hsm(self, x_0, t, gamma=1.0):
-        # “conditioning only on initial data samples x0 and marginalizing over the full initial velocity distribution (as in our hybrid score matching (HSM), see Sec. C), the mean and covariance matrix of the perturbation kernel p(ut|x0) can be obtained by setting μ0 = (x0, 0d)>, Σ0xx = 0, and Σ0vv = γM” ([Dockhorn et al., 2022, p. 21](zotero://select/library/items/EW8U6A8H)) ([pdf](zotero://open-pdf/library/items/NAYANTYJ?page=21&annotation=CW5PUPSH))
+    def get_hsm_kernel_params(self, x_0, t, gamma=1.0):
+        """“conditioning only on initial data samples x0 and marginalizing over the full initial velocity distribution (as in our hybrid score matching (HSM), see Sec. C), the mean and covariance matrix of the perturbation kernel p(ut|x0) can be obtained by setting μ0 = (x0, 0d)>, Σ0xx = 0, and Σ0vv = γM” ([Dockhorn et al., 2022, p. 21](zotero://select/library/items/EW8U6A8H)) ([pdf](zotero://open-pdf/library/items/NAYANTYJ?page=21&annotation=CW5PUPSH))"""
         v_0_hsm = jnp.zeros_like(x_0)
         mean = self.mean(x_0, v_0_hsm, t)
-        cov = self.covariance(t, Sigma_0_xx=0, Sigma_0_vv=gamma*self.M)
+        cov = self.covariance(t, Sigma_0_xx=0, Sigma_0_vv=gamma * self.M)
         return mean, cov
+
+    def compute_l_t(self, Sigma_t):
+        """Compute l_t as defined in:
+        ([pdf](zotero://open-pdf/library/items/NAYANTYJ?page=23&annotation=G4NCGLIX))
+        ([Dockhorn et al., 2022, p. 23](zotero://select/library/items/EW8U6A8H))"""
+        Sigma_xx, Sigma_xv, Sigma_vv = Sigma_t[0, 0], Sigma_t[0, 1], Sigma_t[1, 1]
+        l_t = jnp.sqrt(Sigma_xx / (Sigma_xx * Sigma_vv - Sigma_xv**2))
+        return l_t.item()  # Convert to scalar
+
+    def compute_L_t_inv_T(self, Sigma_t):
+        """Compute L_t^(-T) as defined in:
+        ([pdf](zotero://open-pdf/library/items/NAYANTYJ?page=23&annotation=8M85CFQL))
+        ([Dockhorn et al., 2022, p. 23](zotero://select/library/items/EW8U6A8H))"""
+        Sigma_xx, Sigma_xv, Sigma_vv = Sigma_t[0, 0], Sigma_t[0, 1], Sigma_t[1, 1]
+        sqrt_Sigma_xx = jnp.sqrt(Sigma_xx)
+        sqrt_det = jnp.sqrt(Sigma_xx * Sigma_vv - Sigma_xv**2)
+
+        L_t_inv_T = jnp.array(
+            [
+                [1 / sqrt_Sigma_xx, -Sigma_xv / (sqrt_Sigma_xx * sqrt_det)],
+                [0, sqrt_Sigma_xx / sqrt_det],
+            ]
+        )
+        return L_t_inv_T
+
+    def compute_grad_u_t_log_p_t(self, Sigma_t, epsilon_2d):
+        """Compute the gradient of log p_t(u_t | ·) with respect to u_t."""
+        L_t_inv_T = self.compute_L_t_inv_T(Sigma_t)
+        return -jnp.kron(L_t_inv_T, jnp.eye(self.d)) @ epsilon_2d
+
+    def compute_grad_v_t_log_p_t(self, Sigma_t, epsilon_d):
+        """Compute the gradient of log p_t(u_t | ·) with respect to v_t."""
+        l_t = self.compute_l_t(Sigma_t)
+        return -l_t * epsilon_d
+
+    def compute_dsm_grad_u_t_log_p_t(self, u_0, t, epsilon_2d):
+        """Compute the DSM gradient of log p_t(u_t | u_0)."""
+        _, Sigma_t = self.get_dsm_kernel_params(u_0, t)
+        return self.compute_grad_u_t_log_p_t(Sigma_t, epsilon_2d)
+
+    def compute_hsm_grad_u_t_log_p_t(self, x_0, t, epsilon_2d):
+        """Compute the HSM gradient of log p_t(u_t | x_0)."""
+        _, Sigma_t = self.get_hsm_kernel_params(x_0, t, self.gamma)
+        return self.compute_grad_u_t_log_p_t(Sigma_t, epsilon_2d)
+
+    def compute_dsm_grad_v_t_log_p_t(self, u_t, u_0, t, epsilon_d):
+        """Compute the DSM gradient of log p_t(u_t | u_0) with respect to v_t."""
+        _, Sigma_t = self.get_dsm_kernel_params(u_0, t)
+        return self.compute_grad_v_t_log_p_t(Sigma_t, epsilon_d)
+
+    def compute_hsm_grad_v_t_log_p_t(self, x_0, t, epsilon_d):
+        """Compute the HSM gradient of log p_t(u_t | x_0) with respect to v_t."""
+        _, Sigma_t = self.get_hsm_kernel_params(x_0, t, self.gamma)
+        return self.compute_grad_v_t_log_p_t(Sigma_t, epsilon_d)
--- sde-dev/artifacts/test_cld.py
+++ sde-dev/artifacts/test_cld.py
@@ -8,14 +8,14 @@ from cld import CriticallyDampedLangevinDiffusion
 
 @pytest.fixture
 def cld_instance():
-    return CriticallyDampedLangevinDiffusion(d=10, M=1.0, beta=1.0, T=1.0)
-  
+    return CriticallyDampedLangevinDiffusion(d=10, M=1.0, beta=1.0)
+
 
 def test_initialization(cld_instance):
     assert cld_instance.d == 10
     assert cld_instance.M == 1.0
     assert cld_instance.beta == 1.0
-    assert cld_instance.T == 1.0
+    assert cld_instance.gamma == 1.0
     assert np.isclose(cld_instance.Gamma, np.sqrt(4 * cld_instance.M))
 
 
@@ -63,20 +63,87 @@ def test_covariance_shape(cld_instance):
     assert cov.shape == (2 * cld_instance.d, 2 * cld_instance.d)
 
 
-def test_perturbation_kernel_dsm_shape(cld_instance):
+def test_get_dsm_kernel_params(cld_instance):
     u_0 = np.zeros(2 * cld_instance.d)
     t = 0.5
-    mean, cov = cld_instance.perturbation_kernel_dsm(u_0, t)
+    mean, cov = cld_instance.get_dsm_kernel_params(u_0, t)
     assert mean.shape == (2 * cld_instance.d,)
     assert cov.shape == (2 * cld_instance.d, 2 * cld_instance.d)
 
 
-def test_perturbation_kernel_hsm_shape(cld_instance):
+def test_get_hsm_kernel_params(cld_instance):
     x_0 = np.zeros(cld_instance.d)
     t = 0.5
-    mean, cov = cld_instance.perturbation_kernel_hsm(x_0, t)
+    mean, cov = cld_instance.get_hsm_kernel_params(x_0, t)
     assert mean.shape == (2 * cld_instance.d,)
     assert cov.shape == (2 * cld_instance.d, 2 * cld_instance.d)
+
+
+def test_compute_grad_u_t_log_p_t(cld_instance):
+    Sigma_t = np.eye(2)
+    epsilon_2d = np.random.normal(size=(2 * cld_instance.d,))
+    grad = cld_instance.compute_grad_u_t_log_p_t(Sigma_t, epsilon_2d)
+    assert grad.shape == (2 * cld_instance.d,)
+    assert np.all(np.isfinite(grad))
+
+
+def test_compute_grad_v_t_log_p_t(cld_instance):
+    Sigma_t = np.eye(2)
+    epsilon_d = np.random.normal(size=(cld_instance.d,))
+    grad = cld_instance.compute_grad_v_t_log_p_t(Sigma_t, epsilon_d)
+    assert grad.shape == (cld_instance.d,)
+    assert np.all(np.isfinite(grad))
+
+
+def test_compute_dsm_grad_u_t_log_p_t(cld_instance):
+    u_0 = np.ones(2 * cld_instance.d)
+    t = 0.5
+    epsilon_2d = jax.random.normal(key=jax.random.PRNGKey(0), shape=(2*cld_instance.d,))
+    grad = cld_instance.compute_dsm_grad_u_t_log_p_t(u_0, t, epsilon_2d)
+    assert grad.shape == (2 * cld_instance.d,)
+    assert np.all(np.isfinite(grad))
+
+
+def test_compute_hsm_grad_u_t_log_p_t(cld_instance):
+    x_0 = np.ones(cld_instance.d)
+    t = 0.5
+    epsilon_2d = jax.random.normal(key=jax.random.PRNGKey(0), shape=(2*cld_instance.d,))
+    grad = cld_instance.compute_hsm_grad_u_t_log_p_t(x_0, t, epsilon_2d)
+    assert grad.shape == (2 * cld_instance.d,)
+    assert np.all(np.isfinite(grad))
+
+
+def test_compute_dsm_grad_v_t_log_p_t(cld_instance):
+    u_t = np.zeros(2 * cld_instance.d)
+    u_0 = np.ones(2 * cld_instance.d)
+    t = 0.5
+    epsilon_d = jax.random.normal(key=jax.random.PRNGKey(0), shape=(cld_instance.d,))
+    grad = cld_instance.compute_dsm_grad_v_t_log_p_t(u_t, u_0, t, epsilon_d)
+    assert grad.shape == (cld_instance.d,)
+    assert np.all(np.isfinite(grad))
+
+
+def test_compute_hsm_grad_v_t_log_p_t(cld_instance):
+    x_0 = np.ones(cld_instance.d)
+    t = 0.5
+    epsilon_d = jax.random.normal(key=jax.random.PRNGKey(0), shape=(cld_instance.d,))
+    grad = cld_instance.compute_hsm_grad_v_t_log_p_t(x_0, t, epsilon_d)
+    assert grad.shape == (cld_instance.d,)
+    assert np.all(np.isfinite(grad))
+
+
+def test_compute_l_t(cld_instance):
+    Sigma_t = cld_instance.covariance(1.0, Sigma_0_xx=0, Sigma_0_vv=0)
+    l_t = cld_instance.compute_l_t(Sigma_t)
+    assert np.isscalar(l_t)
+    assert np.isfinite(l_t)
+
+
+def test_compute_L_t_inv_T(cld_instance):
+    Sigma_t = np.array([[1.0, 0.5], [0.5, 2.0]])
+    L_t_inv_T = cld_instance.compute_L_t_inv_T(Sigma_t)
+    assert L_t_inv_T.shape == (2, 2)
+    assert np.all(np.isfinite(L_t_inv_T))
 
 
 def test_invalid_parameters():
@@ -86,9 +153,6 @@ def test_invalid_parameters():
         CriticallyDampedLangevinDiffusion(d=10, M=-1.0, beta=1.0)
     with pytest.raises(ValueError):
         CriticallyDampedLangevinDiffusion(d=10, M=1.0, beta=-1.0)
-    with pytest.raises(ValueError):
-        CriticallyDampedLangevinDiffusion(d=10, M=1.0, beta=1.0, T=-1.0)
-
 
 def test_jit_compilation():
     cld = CriticallyDampedLangevinDiffusion(d=10, M=1.0, beta=1.0)
