6571dbffba139e8dd2a9d7d7d116266f55cbf5da
cyboesch <cyrill.boesch@erdw.ethz.ch>
2024-09-17 19:44:36 -0400

    just added T as a final integration time to the plotting function

--- sde-dev/artifacts/plot_helpers.py
+++ sde-dev/artifacts/plot_helpers.py
@@ -3,12 +3,12 @@ import jax
 import jax.numpy as jnp
 
 
-def plot_time_dependent_energy(energy_fn, n_lines=51, center_x=-2.0, y_min=0.5, y_max=6.0, reverse=False):
+def plot_time_dependent_energy(energy_fn,T=1.0, n_lines=51, center_x=-2.0, y_min=0.5, y_max=6.0, reverse=False):
     # Create a grid of time values
     if reverse:
-        t_values = jnp.linspace(1.0, 0.0, n_lines)
+        t_values = jnp.linspace(1.0*T, 0.0, n_lines)
     else:
-        t_values = jnp.linspace(0.0, 1.0, n_lines)
+        t_values = jnp.linspace(0.0, 1.0*T, n_lines)
 
     x = jnp.linspace(center_x - 6, center_x + 6, 1000)
 
