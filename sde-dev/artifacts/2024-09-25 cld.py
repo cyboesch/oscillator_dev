cc775281449fca2dff2af12ace7992d602958ffd
Geoffrey Roeder <roeder@princeton.edu>
2024-09-25 20:33:40 -0400

    fixes bugs, enhances plots for CTMC

--- sde-dev/artifacts/2024-09-25 cld.py
+++ sde-dev/artifacts/2024-09-25 cld.py
@@ -1,106 +1,114 @@
-#%%
-from typing import Callable
-from pathlib import Path
-from diffrax import Euler, VirtualBrownianTree
+# %%
+# No GPU
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+# %%
 import diffrax
 import jax
+jax.config.update("jax_enable_x64", True)
 import jax.numpy as jnp
 import jax.random as jrnd
 from ctmc import ContinuousTimeMarkovChain
-from mh_solver import MetropolisAdjustedSolver
-from mala import sample as mala_sample
-from jax.scipy.stats import multivariate_normal as MVN
 import matplotlib.pyplot as plt
 from thermoai.distributions import mog_logpdf, sample_mog
-from jaxtyping import Float
-import tqdm
 import seaborn as sns
 
-from mala import compute_log_rho
-
-jax.config.update("jax_enable_x64", True)
 seed = 0
-step_size = 0.22
-# VBT MALA setup
+dt0 = 0.22
 
-# # Create a time-dependent energy function
-# state_dim = 1
-# means = jnp.array([-4.0, 0.0])
-# covariances = jnp.array([1.0, 1.0])
-# weights = jnp.array([0.5, 0.5])
-#- Create a time-dependent energy function
+# - Create a time-dependent energy function
 state_dim = 1
 means = jnp.array([-4.0, 4.0])
 covariances = jnp.array([0.1, 0.1])
 weights = jnp.array([0.1, 0.9])
+
 # - Make time-dependent covariances
-T=10000.0
+T = 10000.0
 t0 = 0.0
 temp_final = 10.0
-
-mass = 1.0*jnp.ones((state_dim,state_dim))
-damping = jnp.sqrt(4.0*mass)
+Zero = jnp.zeros((state_dim, state_dim))
+All_Ones = jnp.ones((state_dim, state_dim))
+Identity = jnp.eye(state_dim)
+mass = 1.0 * Identity
+damping = jnp.sqrt(4.0 * mass)
 
 # --- Make time-dependent weights between t0 and T
 # weights_t_fn = lambda t: jnp.array(
 #     [weights[0] * (1 - (t-t0)/(T-t0)), weights[1] * (t-t0)/(T-t0)]
 # )
 
-
-
 params_fn = lambda t: {
     "means": means,
-    "covariances": covariances*temp_final,
+    "covariances": covariances * temp_final,
     "weights": weights,
 }
 
 initial_position = jnp.zeros(state_dim)
 initial_velocity = jnp.zeros(state_dim)
-initial_state = jnp.concatenate([initial_position, initial_velocity])
+y0 = jnp.concatenate([initial_position, initial_velocity])
+
 target_logdensity_fn = jax.jit(lambda t, x, args: mog_logpdf(x, **params_fn(t)))
-H = lambda t, z, args:  -1.0*target_logdensity_fn(t, z[:state_dim], args) + 0.5*jnp.dot(z[state_dim:],jnp.linalg.inv(mass) @ z[state_dim:])
 
-#%%
-H(0, initial_state, None)
-#%%
+def U(t, z, args):
+    x = z[:state_dim]
+    return -1.0 * target_logdensity_fn(t, x, args)
 
+def V(t, z, args):
+    p = z[state_dim:]
+    return 0.5 * jnp.dot(p, jnp.linalg.inv(mass) @ p)
+
+H = lambda t, z, args: U(t, z, args) + V(t, z, args)
 
 key = jrnd.PRNGKey(seed)
 key, subkey = jrnd.split(key)
 
-bm_key, mh_key = jrnd.split(key)
-bm_vbt = VirtualBrownianTree(t0=t0, t1=T, tol=step_size, shape=(state_dim,), key=key)
-bm_ubt = diffrax.UnsafeBrownianPath(shape=(state_dim,), key=bm_key)
+key, bm_key = jrnd.split(key)
+brownian_motion = diffrax.UnsafeBrownianPath(shape=(2*state_dim,), key=bm_key)
 
-D = lambda z, args: jnp.array([[jnp.zeros((state_dim,state_dim)), jnp.zeros((state_dim,state_dim))], [jnp.zeros((state_dim,state_dim)), damping]]).reshape((2*state_dim,2*state_dim))
-Q = lambda z, args: jnp.array([[jnp.zeros((state_dim,state_dim)), jnp.ones((state_dim,state_dim))], [-jnp.ones((state_dim,state_dim)),jnp.zeros((state_dim,state_dim)) ]]).reshape((2*state_dim,2*state_dim))
+D = lambda z, args: jnp.block(
+    [
+        [Zero, Zero],
+        [Zero, damping * Identity],
+    ]
+)
 
-target_ctmc = ContinuousTimeMarkovChain(H_fn=H, D_fn=D, Q_fn=Q)
+print(f"D shape: {D(y0, None).shape}")
 
-sde_terms_vbt = target_ctmc.get_terms(bm_ubt)
-# mala_solver = MetropolisAdjustedSolver(logdensity_val_and_grad_fn=jax.jit(jax.value_and_grad(target_logdensity_fn, argnums=1)), log_accept_ratio_fn=jax.jit(compute_log_rho), solver=Euler())
-solver = diffrax.ItoMilstein()
+Q = lambda z, args: jnp.block(
+    [
+        [Zero, Identity],
+        [-Identity, Zero],
+    ]
+)
+print(f"Q shape: {Q(y0, None).shape}")
 #%%
+target_ctmc = ContinuousTimeMarkovChain(H_fn=H, D_fn=D, Q_fn=Q)
+target_ctmc_terms = target_ctmc.get_terms(brownian_motion)
+solver = diffrax.ItoMilstein()
+
+# %%
 # Generate samples using VBT MALA
-vbt_solution = diffrax.diffeqsolve(
-    terms=sde_terms_vbt,
+ctmc_solution = diffrax.diffeqsolve(
+    terms=target_ctmc_terms,
     solver=solver,
     t0=t0,
     t1=T,
-    dt0=step_size,
-    y0=initial_state,
+    dt0=dt0,
+    y0=y0,
     args=None,
     adjoint=diffrax.DirectAdjoint(),
-    max_steps=int((T - t0) / step_size) + 1,
-    solver_state=solver.init(sde_terms_vbt,t0, T, initial_state, None),
-    saveat=diffrax.SaveAt(ts=jnp.arange(t0, T, step_size)),
-    progress_meter=diffrax.TqdmProgressMeter()
+    max_steps=int((T - t0) / dt0) + 1,
+    solver_state=solver.init(target_ctmc_terms, t0, T, y0, None),
+    saveat=diffrax.SaveAt(ts=jnp.arange(t0, T, dt0)),
+    progress_meter=diffrax.TqdmProgressMeter(),
 )
-vbt_samples = vbt_solution.ys
-#%%
+ctmc_samples = ctmc_solution.ys
+# %%
 # Import seaborn and update matplotlib style
 import seaborn as sns
 import matplotlib.pyplot as plt
+
 sns.set_theme(style="darkgrid")
 keys = jrnd.split(key, 100000)
 mog_samples = jax.vmap(lambda key: sample_mog(key, **params_fn(0)))(keys)
@@ -109,17 +117,33 @@ mog_samples = jax.vmap(lambda key: sample_mog(key, **params_fn(0)))(keys)
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 10), height_ratios=[2, 1])
 
 # Plot histograms using seaborn on the first subplot
-sns.histplot(vbt_samples.flatten(), kde=True, stat="density", label="VBT MALA Samples", color="skyblue", alpha=0.6, ax=ax1)
-sns.histplot(mog_samples.flatten(), kde=True, stat="density", label="True MoG Samples", color="salmon", alpha=0.6, ax=ax1)
+sns.histplot(
+    ctmc_samples.flatten(),
+    kde=True,
+    stat="density",
+    label="CTMC Samples",
+    color="skyblue",
+    alpha=0.6,
+    ax=ax1,
+)
+sns.histplot(
+    mog_samples.flatten(),
+    kde=True,
+    stat="density",
+    label="True MoG Samples",
+    color="salmon",
+    alpha=0.6,
+    ax=ax1,
+)
 
 # Customize the density plot
-ax1.set_title("Comparison of MALA Samples vs True Mixture of Gaussians", fontsize=16)
+ax1.set_title("Comparison of CTMC Samples vs Directly Sampled Mixture of Gaussians", fontsize=16)
 ax1.set_xlabel("Value", fontsize=12)
 ax1.set_ylabel("Density", fontsize=12)
 ax1.legend(fontsize=10)
 
 # Plot trace plot on the second subplot
-ax2.plot(jnp.arange(len(vbt_samples)), vbt_samples.flatten(), alpha=0.6)
+ax2.plot(jnp.arange(len(ctmc_samples)), ctmc_samples.flatten(), alpha=0.6)
 ax2.set_title("Trace Plot of MALA Samples", fontsize=16)
 ax2.set_xlabel("Sample Index", fontsize=12)
 ax2.set_ylabel("Value", fontsize=12)
--- sde-dev/artifacts/ctmc.py
+++ sde-dev/artifacts/ctmc.py
@@ -8,25 +8,26 @@ from jaxtyping import ArrayLike
 from diffrax._custom_types import Y, Args, RealScalarLike, BM
 
 class ContinuousTimeMarkovChain(eqx.Module):
-    H: Callable[[Y, Args], RealScalarLike]  # H in Fox et al.
-    D: Callable[[Y, Args], ArrayLike]  # D >= 1 in Fox et al.
-    Q: Callable[[Y, Args], ArrayLike]  # Q in Fox et al.
+    H: Callable[[RealScalarLike, Y, Args], RealScalarLike]  # H in Fox et al.
+    D: Callable[[RealScalarLike, Y, Args], ArrayLike]  # D >= 1 in Fox et al.
+    Q: Callable[[RealScalarLike, Y, Args], ArrayLike]  # Q in Fox et al.
+    Gamma: Callable[[RealScalarLike, Y, Args], ArrayLike]  # Correction in Fox et al.
 
     def __init__(
         self,
         H_fn: Callable[[RealScalarLike, Y, Args], RealScalarLike],
-        D_fn: Union[Callable[[Y, Args], ArrayLike], None] = None,
-        Q_fn: Union[Callable[[Y, Args], ArrayLike], None] = None,
-        Gamma_fn: Union[Callable[[Y, Args], ArrayLike], None] = None,
+        D_fn: Union[Callable[[RealScalarLike, Y, Args], ArrayLike], None] = None,
+        Q_fn: Union[Callable[[RealScalarLike, Y, Args], ArrayLike], None] = None,
+        Gamma_fn: Union[Callable[[RealScalarLike, Y, Args], ArrayLike], None] = None,
     ):
         self.H = H_fn
         if D_fn is None:
-            self.D = lambda z, args: jnp.eye(z.shape[-1])
+            self.D = lambda t, z, args: jnp.eye(z.shape[-1])
         else:
             self.D = D_fn
 
         if Q_fn is None:
-            self.Q = lambda z, args: jnp.zeros((z.shape[-1], z.shape[-1]))
+            self.Q = lambda t, z, args: jnp.zeros((z.shape[-1], z.shape[-1]))
         else:
             self.Q = Q_fn
 
@@ -38,25 +39,25 @@ class ContinuousTimeMarkovChain(eqx.Module):
     @eqx.filter_jit
     def drift(self, t: RealScalarLike, z: Y, args: Args) -> ArrayLike:
         grad_H = jax.grad(self.H, argnums=1)(t, z, args)
-        DQ = jnp.add(self.D(z, args), self.Q(z, args))
-        f = -jnp.dot(DQ, grad_H) + self.Gamma(z, args)
+        DQ = jnp.add(self.D(t, z, args), self.Q(t, z, args))
+        f = -jnp.dot(DQ, grad_H) + self.Gamma(t, z, args)
         return f
 
     @eqx.filter_jit
-    def Gamma(self, z: Y, args: Args):
+    def Gamma(self, t: RealScalarLike, z: Y, args: Args):
         # TODO: faster implementation
         def partial_sum(i):
             return jnp.sum(
                 jax.jacfwd(
-                    lambda z, args: self.D(z, args)[i, :] + self.Q(z, args)[i, :]
-                )(z, args)
+                    lambda t, z, args: self.D(t, z, args)[i, :] + self.Q(t, z, args)[i, :]
+                )(t, z, args)
             )
 
         return jax.vmap(partial_sum)(jnp.arange(z.shape[-1]))
 
     @eqx.filter_jit
     def diffusion(self, t: RealScalarLike, z: Y, args: Args) -> ArrayLike:
-        return jnp.sqrt(2 * self.D(z, args))
+        return jnp.sqrt(2 * self.D(t, z, args))
 
     def get_terms(self, bm: BM) -> MultiTerm:
         return MultiTerm(ODETerm(self.drift), ControlTerm(self.diffusion, bm))
@@ -64,8 +65,8 @@ class ContinuousTimeMarkovChain(eqx.Module):
 
 def create_ctmc_from_logdensity(
     logdensity_fn: Callable[[Y, Args], RealScalarLike],
-    D: Union[Callable[[Y, Args], ArrayLike], None] = None,
-    Q: Union[Callable[[Y, Args], ArrayLike], None] = None,
+    D: Union[Callable[[RealScalarLike, Y, Args], ArrayLike], None] = None,
+    Q: Union[Callable[[RealScalarLike, Y, Args], ArrayLike], None] = None,
 ):
-    H = lambda z, args: -1.0 * logdensity_fn(z, args)
+    H = lambda t, z, args: -1.0 * logdensity_fn(t, z, args)
     return ContinuousTimeMarkovChain(H, D, Q)
