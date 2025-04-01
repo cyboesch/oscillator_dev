92944424c5c519c648df51c95626f08bcaca8718
cyboesch <cyrill.boesch@erdw.ethz.ch>
2025-04-01 22:47:04 +0000

    for apptiner to run it on ionic

---
+++ projects/physical-diffusion-models/scripts/submit_job_apptainer.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# --- admin
+#SBATCH --account=lips
+#SBATCH --job-name="MNIST_generation"
+#SBATCH --mail-user=cb7454@princeton.edu
+#SBATCH --mail-type=end
+#SBATCH --time=01:00:00
+#
+# --- resources
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:2
+#SBATCH --mem=64gb
+#SBATCH --output=project_space.out
+
+set -x
+apptainer exec --nv -e --pwd /project container_image.sif python3 workload.py
+exit 0
