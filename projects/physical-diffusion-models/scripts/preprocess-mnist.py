6527f95d673f71b5700e0a5b595a271f60f2a1d3
cyboesch <cb7454@princeton.edu>
2025-05-14 10:37:34 -0400

    ran it to generate 12x12 data

--- projects/physical-diffusion-models/scripts/preprocess-mnist.py
+++ projects/physical-diffusion-models/scripts/preprocess-mnist.py
@@ -59,7 +59,7 @@ def load_and_preprocess_mnist(labels=None, resolution=(8, 8)):
 
 # %%
 labels = [0,1]
-resolution = (10,10)
+resolution = (12,12)
 images, labels_array = load_and_preprocess_mnist(labels=labels, resolution=resolution)
 
 # Plot a few examples
