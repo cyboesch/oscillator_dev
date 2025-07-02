65c039adcd671033f8d1ac9231dc37f866936b87
cyboesch <cb7454@princeton.edu>
2025-07-01 22:09:36 -0400

    increased the t_forward for low temperature sampling

--- projects/physical-diffusion-models/scripts/duffing_network_learning_MNIST_sde.py
+++ projects/physical-diffusion-models/scripts/duffing_network_learning_MNIST_sde.py
@@ -29,12 +29,12 @@ optimization_key, reverse_sde_key, image_noise_added_key = jr.split(master_key, 
 ##################################### 
 # Set parameters
 ##################################### 
-std_of_added_noise = 0.01
+std_of_added_noise = 0.001
 additional_rescaling = 1.
 # Forward process parameters
 Temp = 0.001
-n_time_steps = 15
-t_forward = 4.
+n_time_steps = 20
+t_forward = 10.
 sigma_forward = 1.
 exponential_time_pts = False
 if exponential_time_pts:
