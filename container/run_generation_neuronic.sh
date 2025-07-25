eb731cb2b83e58891bcd3513e814aeab1ee76cf4
Cyrill <cb7454@princeton.edu>
2025-07-25 10:34:48 -0400

    updated runtime limit

--- container/run_generation_neuronic.sh
+++ container/run_generation_neuronic.sh
@@ -5,7 +5,7 @@
 #SBATCH --error=MNIST_generation_%j.err
 #SBATCH --mail-user=cb7454@princeton.edu
 #SBATCH --mail-type=end
-#SBATCH --time=40:00:00
+#SBATCH --time=80:00:00
 #SBATCH --partition=all
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
