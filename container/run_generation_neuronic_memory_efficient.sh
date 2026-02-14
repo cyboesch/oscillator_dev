654071cebbe9c1b72843e8cb3998ee86d375ade2
Cyrill <cb7454@princeton.edu>
2026-02-14 14:21:23 -0500

    reduced the max integration time

--- container/run_generation_neuronic_memory_efficient.sh
+++ container/run_generation_neuronic_memory_efficient.sh
@@ -5,7 +5,7 @@
 #SBATCH --error=MNIST_generation_%j.err
 #SBATCH --mail-user=cb7454@princeton.edu
 #SBATCH --mail-type=end
-#SBATCH --time=400:00:00
+#SBATCH --time=90:00:00
 #SBATCH --partition=all
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
